212d9d13e10356489bf7b0a6660f632afb7f3bab
Stefan Redfield <redfield.stefan@gmail.com>
2015-06-15 15:45:53 -0700

    new function for recipe creation

--- app/js/controllers/all_recipes_controller.js
+++ app/js/controllers/all_recipes_controller.js
@@ -1,12 +1,20 @@
 'use strict';
 
+
+
 module.exports = function(app) {
   app.controller('allRecipesController', ['$scope', function($scope, $http) {
     $scope.errors = [];
     $scope.recipes = [];
+    $scope.ingredients = [];
 
     $scope.getAll = function() {
 
+    }
+
+    $scope.addIngredient = function(ingredient) {
+      $scope.ingredients.push(ingredient)
+      console.log(ingredient);
     }
 
   }]);
