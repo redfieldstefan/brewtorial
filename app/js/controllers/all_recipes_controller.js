a005811922793c1755c99167bd08848304e22eb5
Stefan Redfield <redfield.stefan@gmail.com>
2015-06-16 15:53:57 -0700

    redirect to recipe after creation

--- app/js/controllers/all_recipes_controller.js
+++ app/js/controllers/all_recipes_controller.js
@@ -2,7 +2,7 @@
 
 module.exports = function(app) {
 
-  app.controller('allRecipesController', ['$scope', 'RESTResource', function($scope, resource) {
+  app.controller('allRecipesController', ['$scope', '$location', 'RESTResource', function($scope, $location, resource) {
     var Recipe = resource('recipe');
     $scope.errors = [];
     $scope.recipes = [];
@@ -13,7 +13,7 @@ module.exports = function(app) {
 
 
     $scope.getAll = function() {
-      Brew.getAll(function(err, data) {
+      Recipe.getAll(function(err, data) {
         if(err) {
           $scope.errors.push(err);
           return console.log({msg: 'Dang, error retrieving the recipes'});
@@ -35,6 +35,9 @@ module.exports = function(app) {
           return console.log({msg: 'Dang, error creating the recipe'});
         } else {
           console.log(data);
+          var address = data.result._id;
+          console.log(address);
+          $location.path('/recipe/' + address);
           $scope.errors = [];
           $scope.recipes = [];
           $scope.header = {};
