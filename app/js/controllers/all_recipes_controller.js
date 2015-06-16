a440c4aa744c989277d04e500b46d74917f8b13c
Stefan Redfield <redfield.stefan@gmail.com>
2015-06-15 17:53:20 -0700

    add new functions for create recipe

--- app/js/controllers/all_recipes_controller.js
+++ app/js/controllers/all_recipes_controller.js
@@ -1,20 +1,26 @@
 'use strict';
 
-
-
 module.exports = function(app) {
   app.controller('allRecipesController', ['$scope', function($scope, $http) {
     $scope.errors = [];
     $scope.recipes = [];
     $scope.ingredients = [];
+    $scope.steps = [];
 
     $scope.getAll = function() {
 
     }
 
     $scope.addIngredient = function(ingredient) {
-      $scope.ingredients.push(ingredient)
-    }
+      $scope.ingredients.push({name: ingredient.name, quantity: ingredient.quantity});
+      document.getElementById("ingredientForm").reset();
+    };
+
+    $scope.addStep = function(step) {
+      $scope.steps.push({position: ($scope.steps.length + 1 ), instruction: step.instruction, time: step.time});
+      document.getElementById("stepForm").reset();
+      console.log($scope.steps)
+    };
 
   }]);
 };
