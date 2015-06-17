2275c41ccddaba325e07a4df86b2bc6ad18c78c1
Aaron Martone <am@aaronmartone.com>
2015-06-17 15:40:57 -0700

    Rigged login and cookie clearing logout.

--- app/js/controllers/landing_controller.js
+++ app/js/controllers/landing_controller.js
@@ -5,6 +5,12 @@ module.exports = function(app) {
   LandingController.$inject = ['$scope'];
 
   function LandingController($scope) {
+    var greetings = [
+      'Re-brew-nited<br>and it tastes so good.',
+      'Your Kung-brew<br>is strong, grasshoppa.',
+      'Keep away!<br>I know tae kwon brew!'
+    ];
+    $scope.greeting = greetings[Math.floor(Math.random()*greetings.length)];
     $scope.page = 'landing';
     $scope.tallies = {
       users: 0,
