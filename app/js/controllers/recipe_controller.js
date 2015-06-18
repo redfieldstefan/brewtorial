8b496f43027c4cfa48520d555edc8e9a2c0c23fc
Stefan Redfield <redfield.stefan@gmail.com>
2015-06-18 10:32:34 -0700

    succesfully create a new brew event and redirect user to that events page

--- app/js/controllers/recipe_controller.js
+++ app/js/controllers/recipe_controller.js
@@ -1,7 +1,8 @@
 module.exports = function(app) {
   app.controller('RecipeController', ['$scope', 'RESTResource', '$routeParams', '$location', function($scope, resource, $routeParams, $location) {
     $scope.page = 'recipe';
-    var BrewEvent = resource('brewEvent');
+    var NewBrewEvent = resource('/brew/newbrew');
+
     var Recipe = resource('recipe');
     $scope.errors = [];
     $scope.header;
@@ -14,38 +15,31 @@ module.exports = function(app) {
       Recipe.getOne($routeParams, function(err, data) {
         if (err) {
           console.log(err);
-          $scope.errors.push({msg: 'Problem finding resource'});
+          return $scope.errors.push({msg: 'Problem finding resource'});
         }
-        $scope.recipes = data;
-      });
-    };
-
-    $scope.createBrewEvent = function() {
-      var newBrewEvent = {
-        parentRecipe: $routeParams.id,
-        ingredients: $scope.ingredients,
-        steps: $scope.steps
-      }
-      BrewEvent.create(newBrewEvent, function(err, data){
-        if(err) {
-          $scope.errors.push(err);
-          return console.log({msg: 'Dang, error creating the recipe'});
-        } else {
-          console.log(data);
-          var address = data.result._id;
-          $location.path('/recipe/' + address);
-        }
-      });
         $scope.header = data.result.header;
         $scope.ingredients = data.result.ingredients;
         $scope.equipment = data.result.equipment;
         $scope.steps = data.result.steps;
         $scope.id = data.result._id;
-      };
-
-    $scope.triggerBrewEvent = function(id) {
-      $location.path('/brews/' + id);
+      });
     };
+
+    $scope.createBrewEvent = function() {
+      var newBrew = {
+        title: $scope.header.title,
+        ingredients: $scope.ingredients,
+        steps: $scope.steps,
+      };
+      NewBrewEvent.create(newBrew, function(err, res) {
+        if (err) {
+          console.log(err);
+          return $scope.errors.push({msg: 'Problem finding resource'});
+        }
+        console.log(res);
+        var id = res.data._id;
+        $location.path('/brews/' + id); //NEW CREATE BREW CODE. DOES THIS LOOK RIGHT?
+      });
+    }
   }]);
 };
-
