5e477c7b4f203e3e72399f61f8f616c8482d9dce
Aaron Martone <am@aaronmartone.com>
2015-06-18 10:17:44 -0700

    Landing page tally count tied to database.

--- routes/api/service.js
+++ routes/api/service.js
@@ -1,5 +1,7 @@
 // import models.
 var User = require('../../models/User');
+var Recipe = require('../../models/Recipe');
+var Events = require('../../models/BrewEvent');
 
 // export router.
 module.exports = function(router) {
@@ -10,16 +12,58 @@ module.exports = function(router) {
 
         // get the tallies displayed on the landing page.
         case 'getLandingTallies':
-          res.status(200)
-            .json({
-              success: true,
-              message: 'Tallies successfully queried.',
-              result: {
-                users: 10,
-                recipes: 20,
-                craftings: 100  
-              }              
-            });
+          var results = {
+            users: 0,
+            recipes: 0,
+            craftings: 0
+          };
+          var asyncCompleted = 0;
+
+          // get users tally.
+          User.count({}, function(err, count) {
+            if (err) {
+              console.log('error getting users tally', err);
+              res.status(500)
+                .json({ msg: 'internal server error' });
+            }
+            results.users = count;
+            sendResultCheck();
+          });
+
+          // get recipes tally.
+          Recipe.count({}, function(err, count) {
+            if (err) {
+              console.log('error getting recipes tally', err);
+              res.status(500)
+                .json({ msg: 'internal server error' });
+            }
+            results.recipes = count;
+            sendResultCheck();
+          });
+
+          // get craftings (events) tally.
+          Events.count({}, function(err, count) {
+            if (err) {
+              console.log('error getting craftings tally', err);
+              res.status(500)
+                .json({ msg: 'internal server error' });
+            }
+            results.craftings = count;
+            sendResultCheck();
+          })
+
+          function sendResultCheck() {
+            asyncCompleted++;
+            if (asyncCompleted === 3) {
+              res.status(200)
+                .json({
+                  success: true,
+                  message: 'Tallies successfully queried.',
+                  result: results   
+                });
+            }
+          }
+                    
           break;
 
         // ensures email address provided is unique.
