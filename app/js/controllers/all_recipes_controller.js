86db142e6ef542023e46ed3efc056d6d0c23e5cd
Andrew Jacobson <andyjacuw@gmail.com>
2015-06-18 10:34:11 -0700

    Move recipe endpoint to 'recipes'

---
+++ app/js/controllers/all_recipes_controller.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = function(app) {
+
+  AllRecipesController.$inject = ['$scope', '$location', 'RESTResource'];
+
+  function AllRecipesController($scope, $location, resource) {
+    $scope.page = 'recipe';
+    var Recipe = resource('recipe');
+    $scope.errors = [];
+    $scope.recipes = [];
+    $scope.beerTally = 0;
+    $scope.sortOrder = false;
+    $scope.sortBy = 'name';
+
+    $scope.getAll = function() {
+      Recipe.getAll(function(err, data) {
+        if (err) {
+          console.log(err);
+          $scope.errors.push({msg: 'unable to retrieve recipes'});
+        }
+
+        $scope.beerTally = data.result.length;
+        $scope.recipes = data.result;
+      });
+    };
+
+    $scope.sort = function(propName) {
+      $scope.sortBy = 'header.' + propName;
+      $scope.sortOrder = !$scope.sortOrder;
+    };
+
+    $scope.view = function(recipe) {
+      $location.path('recipes/' + recipe._id);
+    }
+  };
+
+  app.controller('AllRecipesController', AllRecipesController);
+};
