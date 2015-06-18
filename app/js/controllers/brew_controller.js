94b4e384bbf7e4b509d6a81672773fd36749a03e
Stefan Redfield <redfield.stefan@gmail.com>
2015-06-17 18:05:06 -0700

    new functions in brew controller

--- app/js/controllers/brew_controller.js
+++ app/js/controllers/brew_controller.js
@@ -2,20 +2,37 @@
 
 module.exports = function(app) {
 
-  app.controller('BrewController', ['$scope', '$location', 'RESTResource', function($scope, $location, resource) {
+  app.controller('BrewController', ['$scope', '$location', '$routeParams', 'RESTResource', function($scope, $location, $routeParams, resource) {
     var Brew = resource('brew');
     $scope.errors = [];
     $scope.ingredients = [{item: 'Test Ingredient', amount: 5, unit: 'Cups'}, {item: 'Test Ingredient', amount: 5, unit: 'Oz'}, {item: 'Test Ingredient', amount: 5, unit: 'Lbs'}];
-    $scope.steps = [{directions: 'Some Test Directions', offset: 60, status: true}, {directions: 'Some Test Directions', offset: 60, status: false}];
+    $scope.steps = [{directions: 'Some Test Directions', offset: 60, status: true}, {directions: 'Some Test Directions', offset: 60, status: false}, {directions: 'Some Test Directions', offset: 60, status: false}];
     $scope.title = '';
 
     $scope.getBrew = function(id) {
+      Brew.getOne($routeParams, function(err, data) {
+        if(err){
+           $scope.errors.push(err);
+          return console.log({msg: 'Dang, error creating the recipe'});
+        }
+        console.log(data);
+        $scope.steps.push(data.steps);
+        $scope.ingredients.push(data.ingredients);
+        $scope.title = data.title;
+      });
     };
 
-    $scope.startTime = function() {
+    $scope.startStep = function(step) {
+      step.status = true;
+    }
+
+    $scope.startTime = function(time) {
     };
 
-    $scope.stopTime = function(){
+    $scope.stopTime = function(time){
+    };
+
+    $scope.resetTime = function(time){
     };
 
 
