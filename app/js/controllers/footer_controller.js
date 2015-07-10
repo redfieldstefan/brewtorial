4d9361f72e3d80e9eb6393ef2dd376f0f80623a8
Stefan Redfield <redfield.stefan@gmail.com>
2015-07-09 19:01:30 -0700

    make footer links clickeable

--- app/js/controllers/footer_controller.js
+++ app/js/controllers/footer_controller.js
@@ -3,5 +3,8 @@
 module.exports = function(app) {
   app.controller('footerController', ['$scope', '$location', function($scope, $location){
 
+    $scope.redirect = function(destination) {
+      $location.path('/' + destination);
+    }
   }]);
 };
