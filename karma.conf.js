62e4cb42d0ae771d369e70089e5b90dccbf442c3
David Davidson <david.davidson@formidable.com>
2016-04-04 19:55:37 -0700

    wip (rebase): html2js

--- karma.conf.js
+++ karma.conf.js
@@ -13,10 +13,22 @@ module.exports = function(config) {
     frameworks: ['jasmine', 'sinon'],
 
 
+    ngHtml2JsPreprocessor: {
+      moduleName: 'templates'
+    },
+
     // list of files / patterns to load in the browser
     files: [
       './node_modules/phantomjs-polyfill/bind-polyfill.js',
-      'test/karma_tests/bundle.js'
+      'test/karma_tests/bundle.js',
+      'client/views/**/*.html'
+    ],
+
+    plugins: [
+      'karma-sinon',
+      'karma-phantomjs-launcher',
+      'karma-jasmine',
+      'karma-ng-html2js-preprocessor'
     ],
 
 
@@ -28,6 +40,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'client/views/**/*.html': ['ng-html2js']
     },
 
 
@@ -47,7 +60,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
 
 
     // enable / disable watching file and executing tests whenever any file changes
